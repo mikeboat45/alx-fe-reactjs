05ac5ab8e40ce09013224983d1fc0b56e1939510
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-09-07 21:26:48 +0000

    counter.jsx correction

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,8 +6,8 @@ export default function Counter() {
 	return (
 	<>
 	<p>Current score: {count}</p>
-	<button style={{backgroundColor:'green'}} onClick={() => setCount(count + 1)}>Increase</button>
-	<button style={{backgroundColor:'red'}} onClick={() => setCount(count - 1)}>Decrease</button>
+	<button style={{backgroundColor:'green'}} onClick={() => setCount(count + 1)}>Increment</button>
+	<button style={{backgroundColor:'red'}} onClick={() => setCount(count - 1)}>Decrement</button>
 	<button style={{backgroundColor:'gold'}} onClick={() => setCount(0)}>Reset</button>
 	</>
 	);
