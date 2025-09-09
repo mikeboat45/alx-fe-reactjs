a28c2b2ff0a1dff4be392176783c11c53ef21e53
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-09-09 17:09:37 +0000

    fix: error handling bugs

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -20,9 +20,9 @@ export default function App() {
 
 			<Routes>
 				<Route path="/" element={<Home />} />
-        		<Route path="./About" element={<About />} />
-				<Route path="./Services" element={<Services />} />
-        		<Route path="./Contact" element={<Contact />} />
+        		<Route path="/About" element={<About />} />
+				<Route path="/Services" element={<Services />} />
+        		<Route path="/Contact" element={<Contact />} />
 				<Route path="*" element={<ErrorPage />} />
 			</Routes>
 		</>
--- my-company/src/error-page.jsx
+++ my-company/src/error-page.jsx
@@ -1,7 +1,7 @@
-import { useRouteError } from "react-router-dom";
+import React from "react";
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  let error = new Error("Page not found");
   console.error(error);
 
   return (
