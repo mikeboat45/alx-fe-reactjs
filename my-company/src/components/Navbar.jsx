e5a520c828840bb33c000affa08242aca44b3809
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-09-09 17:53:46 +0000

    update: add inline styles

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
 	return (
-		<nav>
+		<nav styles={{backgroundColor: '#f8f9fa', justifyContent: 'space-between', display: 'flex'}}>
 			<Link to='./Home' className='nav-link'>Home</Link>
 			<Link to="./About" className='nav-link'>About</Link>
 			<Link to="./Services" className='nav-link'>Services</Link>
