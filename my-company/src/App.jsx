858346ebd0275d2f2c3e72f71f7f3c95de181a7d
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-09-09 16:47:47 +0000

    feat: implement routing

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Services from './Services'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	return (
+		<>
+			<nav>
+				<ul>
+					<li><Link to='./Home'>Home</Link></li>
+					<li><Link to="./About">About</Link></li>
+					<li><Link to="./Services">Services</Link></li>
+          			<li><Link to="./Contact">Contact</Link></li>
+				</ul>
+			</nav>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			<Routes>
+				<Route path="/" element={<Home />} />
+        		<Route path="./About" element={<About />} />
+				<Route path="./Services" element={<Services />} />
+        		<Route path="./Contact" element={<Contact />} />
+			</Routes>
+		</>
+	);
+
 }
 
-export default App
