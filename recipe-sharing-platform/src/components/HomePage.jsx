5e8f7b82424ead3a677a30919a91b5074d413c4e
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-10-03 14:02:16 +0000

    tired

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,15 +1,31 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function HomePage(props) {
+  const [details, setDetails] = useState({
+    image: props.image,
+    title: props.title,
+    summary: props.summary,
+  });
+
+  useEffect(() => {
+    setDetails({
+      image: props.image,
+      title: props.title,
+      summary: props.summary,
+    });
+  }, [props.image, props.title, props.summary]);
+
   return (
-    <article className="border-2 border-black rounded flex flex-row m-8">
-      <div>
-        <img src={props.image} alt="placeholder" className="rounded" />
-      </div>
-      <div className="m-2">
-        <h2 className="font-semibold text-xl mb-2">{props.title}</h2>
-        <p>{props.summary}</p>
-      </div>
-    </article>
+    <div className="grid grid-rows">
+      <article className="border-2 border-black rounded flex m-8">
+        <div className="flex-shrink-0">
+          <img src={details.image} alt="placeholder" className="rounded" />
+        </div>
+        <div className="m-2">
+          <h2 className="font-semibold text-xl mb-2">{details.title}</h2>
+          <p>{details.summary}</p>
+        </div>
+      </article>
+    </div>
   );
 }
